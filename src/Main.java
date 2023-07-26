b4af21d8735a445fb1cf85888e11b06a5c1cc54a
Stephane YOT <merlinstephane9@gmail.com>
2023-07-26 20:55:23 +0200

    repos du menu

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ package cohort2;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Menu {
+public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
